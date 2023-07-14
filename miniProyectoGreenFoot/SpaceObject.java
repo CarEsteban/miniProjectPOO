31cbab15b7a32d5b81c9c8cac89996b7d3d11f35
Nicolás <con23197@uvg.edu.gt>
2023-07-14 07:41:03 -0600

    Comit 1-Creacion de imagenes
    Creación de imagenes y subclases

---
+++ miniProyectoGreenFoot/SpaceObject.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SpaceObject here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SpaceObject extends Actor
+{
+    /**
+     * Act - do whatever the SpaceObject wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
